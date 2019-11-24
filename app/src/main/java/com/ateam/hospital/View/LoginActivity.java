6895f1349a379623bd9fee17262372b47475f298
Anchit Gupta <anchitgupt2012@gail.com>
2019-11-24 22:56:57 +0530

    Initial commit

---
+++ app/src/main/java/com/ateam/hospital/View/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.ateam.hospital.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ateam.hospital.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
